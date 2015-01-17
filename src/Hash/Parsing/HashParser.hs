5d91f29d402ea09512691c0a77f37dcacecbc29f
yossarin <vlatko.klabucar@gmail.com>
2015-01-17 06:57:49 +0100

    Parser almost finished. TODO: if, if-then-else, while, tlexpr parsers.

--- src/Hash/Parsing/HashParser.hs
+++ src/Hash/Parsing/HashParser.hs
@@ -1,15 +1,18 @@
 module Hash.Parsing.HashParser where
 
 import Hash.Language.Expressions
+import Text.Parsec.Expr
 import Text.Parsec.String (Parser)
-import Text.Parsec.Char (noneOf, digit, char, letter, spaces, alphaNum)
-import Text.Parsec (parse, ParseError, try, optionMaybe, option)
-import Text.Parsec.Combinator (many1, sepBy1)
+import Text.Parsec.Char (string, oneOf, noneOf, digit, char, letter, spaces, alphaNum)
+import Text.Parsec (parse, ParseError, try, optionMaybe, option, choice, skipMany1)
+import Text.Parsec.Combinator (many1, sepBy1, between)
 import Control.Applicative ((<$>), (<$), (<*>), (<*), (*>), (<|>), Applicative, many)
 import Data.Char (digitToInt)
 import Control.Monad (liftM)
 
 err = "An error has occurred"
+
+keywords = ["if", "fi", "then", "else"]
 
 simpleParse :: Parser a -> String -> Either ParseError a
 simpleParse p = parse p err
@@ -35,6 +38,10 @@ integer = number <|> negative
 token :: Parser a -> Parser a
 token = (<* spaces)
 
+-- Applicative of this parser disregards prepending spaces
+sp :: Parser a -> Parser a
+sp = (spaces *>)
+
 -- Parses a single character
 symbol :: Char -> Parser Char
 symbol = token . char
@@ -48,9 +55,9 @@ betterParse p = parse (spaces *> p) err
 integer' :: Parser Int
 integer' = token integer
 
--- Parsing a string
-string :: Parser Expr
-string = Str <$> (token . many1 $ noneOf "\n\" <>[]{}();")
+-- Parsing a string of characters
+stringOfChars :: Parser Expr
+stringOfChars = Str <$> (token . many1 $ noneOf "\n\" <>[]{}();")
 
 -- Parsing the variable name, first character must be a letter,
 -- the rest can be alphaNum
@@ -64,7 +71,7 @@ stringLiteral = Str <$> (char '"' *> many (noneOf "\"") <* char '"')
 -- Parses either a variable name, string of characters or 
 -- string snclosed in "double quotes"
 expression :: Parser Expr
-expression = try string <|> stringLiteral <|> variable 
+expression = try stringOfChars <|> stringLiteral <|> variable 
 
 -- Parses the assignemt to a variabl
 assignment :: Parser Cmd
@@ -109,3 +116,49 @@ commandAppending = do
 -- Cmd parser (if there is no output redirect it defaults to non-appending)
 command :: Parser Cmd
 command = try commandAppending <|> commandNonAppending
+
+-- Parser for command or assigment
+cmd :: Parser Cmd
+cmd = try assignment <|> command
+
+-- Parses a comparison operator (==, /=, <, >, >=, <=)
+compOp :: Parser String 
+compOp = token . many1 $ oneOf "=/<>"
+
+-- Returns a corresponding comparrison operator
+strToOperator :: Expr -> String -> Expr -> Comp
+strToOperator e1 s e2 = case s of
+  "==" -> CEQ e1 e2
+  "/=" -> CNE e1 e2
+  ">=" -> CGE e1 e2
+  ">"  -> CGT e1 e2
+  "<=" -> CLE e1 e2
+  "<"  -> CLT e1 e2
+
+-- Parsing comparison of expressions
+comp :: Parser Comp
+comp = strToOperator <$> expression <*> compOp <*> expression
+
+-- Table of logical operators
+table = [[unary '!' Not, binary '|' Or, binary '&' And]]
+  where binary name f = Infix  (f <$ (symbol name)) AssocLeft
+        unary  name f = Prefix (f <$ (symbol name))
+
+-- Parser for logic expressions
+predicates :: Parser Pred
+predicates = buildExpressionParser table other
+  where other       = rootCase <|> parenthases
+        rootCase    = Pred <$> (token $ comp)
+        parenthases = Parens <$> (char '(' *> spaces *> predicates <* char ')' <* spaces)
+
+-- If parser
+
+-- If-Else parser
+ifparser :: Parser Conditional
+ifparser = If <$> (string "if" *> sp predicates) <*> (sp $ string "then" *> (sp $ char '{') *> many (sp cmd <* char ';' <* spaces) <* (sp $ char '}'))
+
+-- If-Then-Else parser
+
+-- While loop parser
+
+-- TLExpr parser
