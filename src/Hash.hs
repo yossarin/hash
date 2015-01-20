04302466e6507d9c64b5e271a7782a0d0a826652
yossarin <vlatko.klabucar@gmail.com>
2015-01-20 07:34:46 +0100

    Implemented REPL.

--- src/Hash.hs
+++ src/Hash.hs
@@ -3,10 +3,13 @@ module Hash
   , runInteractive
   ) where
 
+import System.IO (hFlush, stdout)
 import Hash.Language.Exec
 import Hash.Language.Commands
 import Hash.Parsing.HashParser (parseToTLExpr) 
 import System.Directory (getCurrentDirectory)
+import qualified Data.Map as M
+import Control.Exception
 
 -- The top-level module. Connects parsing to execution and adds interaction
 -- with the user / reading from file.
@@ -21,5 +24,27 @@ runScript fp = do
 
 -- Communicates with the user and performs hash commands line by line
 runInteractive :: IO ()
-runInteractive = putStrLn "TODO: runInteractive"
+runInteractive = do
+  currentDir <- getCurrentDirectory
+  repl (ScriptState {output = "", wd = currentDir, vartable = M.empty}) "hash-0.1> "
 
+-- flushStr prints out a string and immediately flushes the stream
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+-- Prints out a prompt and reads in a line of input
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+-- Read Eval Print Loop
+repl :: ScriptState -> String -> IO ()
+repl ss prompt = do
+  line <- readPrompt prompt
+  case line of
+    ":q" -> return ()
+    _    -> do
+     ss' <- catch (runHashProgram commands (Right ss) (parseToTLExpr line)) $
+              \e -> do
+                putStrLn $ " *** Exception: " ++ show (e :: SomeException)
+                return ss
+     repl ss' prompt
--- src/Hash/Language/Commands.hs
+++ src/Hash/Language/Commands.hs
@@ -6,9 +6,73 @@ import System.Directory
 
 -- A map of (command name, command pairs), used to abstract command
 -- execution and make adding new commands relatively easy
-commands = M.fromList [("pwd", pwd)]
+commands = M.fromList [ ("pwd",    pwd)
+                      , ("mv",     mv )
+                      , ("cp",     cp )
+                      , ("rm",     rm )
+                      , ("create", create)
+                      ]
 
 pwd :: Command
 pwd _ ss = do
   currentDir <- getCurrentDirectory
-  return $ ss { output = currentDir} 
+  return $ ss { output = currentDir}
+
+{-
+   Basic file manipulation commands mv, cp, rm, create.
+   They support both absolute and relative paths.
+-}
+
+-- Appends filename to directory
+apf :: FilePath -> FilePath -> FilePath
+apf dir file | last dir == '/' = dir ++ file
+                    | otherwise       = dir ++ "/" ++ file
+
+-- Gets filename from FilePath
+getFilename :: FilePath -> FilePath
+getFilename = reverse . takeWhile (/= '/') . reverse
+
+mv :: Command
+mv (src:dest:_) ss = do
+  srcEx <- doesFileExist src
+  if srcEx then do
+    destDir <- doesDirectoryExist dest
+    if destDir then do
+      copyFile src (apf dest (getFilename src))
+      removeFile src
+      return $ ss { output = "" }
+    else do
+      copyFile src dest
+      removeFile src
+      return $ ss { output = "" }
+  else return $ ss { output = ("Error: file '" ++ src ++ "' does not exist :(") }
+
+cp :: Command
+cp (src:dest:_) ss = do
+  srcEx <- doesFileExist src
+  if srcEx then do
+    destDir <- doesDirectoryExist dest
+    if destDir then do
+      copyFile src (apf dest (getFilename src))
+      return $ ss { output = "" }
+    else do
+      copyFile src dest
+      return $ ss { output = "" }
+  else return $ ss { output = ("Error: file '" ++ src ++ "' does not exist :(") }
+
+rm :: Command
+rm []       ss = return ss
+rm (f:args) ss = do
+  dfe <- doesFileExist f
+  let o = output ss
+  if dfe then do
+    removeFile f
+    rm args $ ss { output = o ++ "\nRemoved file '" ++ f ++ "'."}
+  else return $ ss { output = o ++ "\nError: '" ++ f ++ "' : no such file :("}
+
+create :: Command
+create []     ss = return ss
+create (f:fs) ss = do
+  writeFile f ""
+  let o = output ss
+  create fs $ ss { output = o ++ "\nCreated file '" ++ f ++ "'."}
